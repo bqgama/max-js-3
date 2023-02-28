7e47b01c9abb5dab1209579bba2af5a530003eda
bqgama <bqgama@gmail.com>
2023-02-27 22:07:55 -0300

    feature: created a wrapper component; used react.fragments; learned about portals

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import Button from "../UI/Button";
+import Wrapper from "../Helpers/Wrapper";
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
@@ -52,8 +53,13 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler} />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -71,7 +77,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
